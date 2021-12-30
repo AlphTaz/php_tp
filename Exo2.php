7ad433126e93084447f37e72d02edf1800d7e8dc
Bérénice BRUOT <bebe97270@gmail.com>
2021-12-30 16:46:15 +0100

    V1

---
+++ Exo2.php
@@ -0,0 +1,19 @@
+<?php 
+
+// Fontion de détection d'un pallindrome
+
+$string ="kayak";
+$reverse =strrev($string);
+
+if ($string = $reverse) {
+    echo ("C'est un pallindrome");
+}
+
+// ------------------------------- 
+
+// Fontion de détection d'un anagrame 
+
+$string ="kayak";
+$reverse =strrev($string);
+
+?>
